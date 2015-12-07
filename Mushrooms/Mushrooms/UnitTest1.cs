e19089f2956e46432b7a70dc56d9079a2c564748
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-08 00:15:54 +0200

    Mushrooms Test

--- Mushrooms/Mushrooms/UnitTest1.cs
+++ Mushrooms/Mushrooms/UnitTest1.cs
@@ -9,6 +9,15 @@ namespace Mushrooms
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(10, CalculateMusRed(15,2));
+            Assert.AreEqual(250, CalculateMusRed(300, 5));
+        }
+
+        float CalculateMusRed(int number,int x)
+        {
+            float a = (number) / (1 + x);
+            return number - a;
+
         }
     }
 }
