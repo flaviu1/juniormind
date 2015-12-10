066a7758f0ef09d24c23af1474997d447607641c
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-10 23:54:37 +0200

    PavageTest

---
+++ Parquet/Parquet/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parquet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, CalculateParquet(100, 100, 10, 100));
+        }
+
+        int CalculateParquet(int lenghtRoom, int widthRoom, int lenghtParquet, int widthParquet)
+        {
+            float Board = (lenghtRoom * widthRoom + (15 / 100) * lenghtRoom * widthRoom) / (lenghtParquet * widthParquet);
+            return (int)Board;
+        }
+
+    }
+}
