8c4b0723d81a8758520b9585387557da9321c48b
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-24 14:57:46 +0200

    CalculateKilogramsEvenEndOdd

--- TestMelone/TestMelone/UnitTest1.cs
+++ TestMelone/TestMelone/UnitTest1.cs
@@ -11,6 +11,15 @@ namespace TestMelone
         {
             Assert.AreEqual(5, CalculateMeloneForFriends(10));
         }
+        [TestMethod]
+        public void ForKilogramsEven()
+        {
+            Assert.AreEqual(5, CalculateMeloneForFriends(10));
+        } 
+        public void ForOdd()
+        {
+            Assert.AreEqual(1, CalculateMeloneForFriends(11));
+        }
 
         int CalculateMeloneForFriends(int Kilograms)
         {
