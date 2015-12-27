f532b5e960ff4b6b33c1a3b1d6c4196183fcee75
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-28 01:14:06 +0200

    FunctionConvertNumberRoman

--- NumeralsRoman/NumeralsRoman/UnitTest1.cs
+++ NumeralsRoman/NumeralsRoman/UnitTest1.cs
@@ -9,11 +9,23 @@ namespace NumeralsRoman
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("a", Convertnumber(55));
+            Assert.AreEqual("L", ConvertnumberRoman(50));
+
         }
-        String Convertnumber(int number)
+        String ConvertnumberRoman(int number)
         {
-            return "a";
+            if ((number < 0) || (number > 100)) 
+            if (number < 1) return string.Empty;
+            if (number >= 100) return "C";
+            if (number >= 90) return "XC" ;
+            if (number >= 50) return "L" ;
+            if (number >= 40) return "XL";
+            if (number >= 10) return "X" ;
+            if (number >= 9) return "IX" ;
+            if (number >= 5) return "V" ;
+            if (number >= 4) return "IV";
+            if (number >= 1) return "I";
+            else return "Empty" ;
         }
     }
 }
