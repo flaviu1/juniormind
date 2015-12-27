bdebdddfe3f3d01845e0f2b2a8dad20933188cdb
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-28 00:59:01 +0200

    EmptyNumeralsRoman

---
+++ NumeralsRoman/NumeralsRoman/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumeralsRoman
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+
+        }
+        String Convertnumber(int number)
+        {
+            return "a";
+        }
+    }
+}
