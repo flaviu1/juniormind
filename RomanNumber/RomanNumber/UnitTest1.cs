f9c6f07a5e22285675c6195db1ade720bca7ce98
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-30 14:15:07 +0200

    ChangeFunctionToRoman

--- RomanNumber/RomanNumber/UnitTest1.cs
+++ RomanNumber/RomanNumber/UnitTest1.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace NumeralsRoman
+namespace RomanNumber
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForNumberRomanWithDrop()
         {
-            Assert.AreEqual("XCIX", ToRoman(99));
-            Assert.AreEqual("XCIX", IntToRoman(99));
+            Assert.AreEqual("MMMDLV", ToRoman(3555));
+            Assert.AreEqual("MMMDLV", IntToRoman(3555));
         }
         [TestMethod]
-      public void TestMethod2()
+        public void TestForNumberRomanWithDescomposition()
         {
-            Assert.AreEqual("LXXVII", ToRoman(77));
-          
+            Assert.AreEqual("MMCMXCIII", ToRoman(2993));
+            Assert.AreEqual("MMCMXCIII", IntToRoman(2993));
         }
         public static string ToRoman(int number)
         {
             if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value betwheen 1 and 3999");
             if (number < 1) return string.Empty;
             if (number >= 1000) return "M" + ToRoman(number - 1000);
-            if (number >= 900) return "CM" + ToRoman(number - 900); 
+            if (number >= 900) return "CM" + ToRoman(number - 900); //EDIT: i've typed 400 instead 900
             if (number >= 500) return "D" + ToRoman(number - 500);
             if (number >= 400) return "CD" + ToRoman(number - 400);
             if (number >= 100) return "C" + ToRoman(number - 100);
@@ -37,9 +37,19 @@ namespace NumeralsRoman
             if (number >= 1) return "I" + ToRoman(number - 1);
             throw new ArgumentOutOfRangeException("something bad happened");
         }
-         String IntToRoman(int number)
+        public string IntToRoman(int number)
         {
-            return "XCIX";
+            string[] thou = { "", "M", "MM", "MMM" };
+            string[] hun = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] ten = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string roman = "";
+            roman += thou[(int)(number / 1000) % 10];
+            roman += hun[(int)(number / 100) % 10];
+            roman += ten[(int)(number / 10) % 10];
+            roman += ones[number % 10];
+
+            return roman;
         }
     }
 }
