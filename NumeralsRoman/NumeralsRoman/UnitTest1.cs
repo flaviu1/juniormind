cfa4d7aaaef18db1d75697b2681ded04f209f12e
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-30 13:48:09 +0200

    AddToFunctionDescompozition

--- NumeralsRoman/NumeralsRoman/UnitTest1.cs
+++ NumeralsRoman/NumeralsRoman/UnitTest1.cs
@@ -9,43 +9,41 @@ namespace NumeralsRoman
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("C", ConvertnumberRoman(100));
+            Assert.AreEqual("XCIX", ConvertnumberRomanDrop(99));
+            Assert.AreEqual("XCIX", ConvertnumberRomanDescompozition(99));
         }
         [TestMethod]
-        public void TestMethod2()
+      public void TestMethod4()
         {
-            Assert.AreEqual("L", ConvertnumberRoman(50));
-        }
-        [TestMethod]
-        public void TestMethod3()
-        {
-            Assert.AreEqual("XLV", ConvertnumberRoman(45));
-        }
-        [TestMethod]
-        public void TestMethod4()
-        {
-            Assert.AreEqual("Empty", ConvertnumberRoman(101));
+            Assert.AreEqual("LXXVII", ConvertnumberRomanDrop(77));
+           // Assert.AreEqual("LXXVII", ConvertnumberRomanDescompozition(77));
         }
         string x;
-        String ConvertnumberRoman(int number)
-        { 
-            if ((number < 0) || (number > 100)) return "Empty" ;
-            if (number == 100) { x = x + "C";   number = number - 100; }
-            if (number >= 90)  { x = x + "XC";  number = number - 90;  }
-            if (number >= 50)  { x = x + "L";   number = number - 50;  }
-            if (number >= 40)  { x = x + "XL";  number = number - 40;  }
-            if (number >= 20)  { x = x + "XX";  number = number - 20;  }
-            if (number >= 10)  { x = x + "X";   number = number - 10;  }
-            if (number >= 9)   { x = x + "IX";  number = number - 9;   }
-            if (number >= 8)   { x = x + "VIII";number = number - 8;   }
-            if (number >= 7)   { x = x + "VII"; number = number - 7;   }
-            if (number >= 6)   { x = x + "VI";  number = number - 6;   }
-            if (number >= 5)   { x = x + "V";   number = number - 5;   }
-            if (number >= 4)   { x = x + "IV";  number = number - 4;   }
-            if (number >= 3)   { x = x + "III"; number = number - 3;   }
-            if (number >= 2)   { x = x + "II";  number = number - 2;   }
-            if (number >= 1)   { x = x + "I";   number = number - 1;   }
+        String ConvertnumberRomanDrop(int number)
+        {
+            while (number > 0)
+            {
+                if ((number < 0) && (number >= 100)) return "Empty";
+                if (number >= 90) { x = x + "XC"; number = number - 90; }
+                if (number >= 50) { x = x + "L"; number = number - 50; }
+                if (number >= 40) { x = x + "XL"; number = number - 40; }
+                if (number >= 20) { x = x + "XX"; number = number - 20; }
+                if (number >= 10) { x = x + "X"; number = number - 10; }
+                if (number >= 9) { x = x + "IX"; number = number - 9; }
+                if (number >= 8) { x = x + "VIII"; number = number - 8; }
+                if (number >= 7) { x = x + "VII"; number = number - 7; }
+                if (number >= 6) { x = x + "VI"; number = number - 6; }
+                if (number >= 5) { x = x + "V"; number = number - 5; }
+                if (number >= 4) { x = x + "IV"; number = number - 4; }
+                if (number >= 3) { x = x + "III"; number = number - 3; }
+                if (number >= 2) { x = x + "II"; number = number - 2; }
+                if (number >= 1) { x = x + "I"; number = number - 1; }
+            }
             return x;
+        }
+         String ConvertnumberRomanDescompozition(int number)
+        {
+            return "XCIX";
         }
     }
 }
