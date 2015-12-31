e75b9c93c9cde5563c00c37a43641aa0db0d20b6
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-31 16:49:57 +0200

    SolutionForElevenToThirtyDay

--- DeptRent/DeptRent/UnitTest1.cs
+++ DeptRent/DeptRent/UnitTest1.cs
@@ -14,14 +14,15 @@ namespace DeptRent
         [TestMethod]
         public void PenalityForElevenToThirtyDay()
         {
-            Assert.AreEqual(104, CalculateDeptRent(100, 2));
+            Assert.AreEqual(200, CalculateDeptRent(100, 20));
         }
-
+        decimal rentDelay;
         decimal CalculateDeptRent(decimal price ,int day)
         {
-            decimal priceForMaximumTenAay;
-            priceForMaximumTenAay = price + ((2 * price) / 100 * day);
-            return priceForMaximumTenAay;
+            if ((day >= 1) || (day<= 10)) rentDelay = price + ((2 * price) / 100 * day);
+            if ((day >= 11) || (day >= 30)) rentDelay = price + ((5 * price) / 100 * day);
+
+            return rentDelay ;
            
         }
 
