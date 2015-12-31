e97608219bd9b8a916dc883bb0df3ccd369db432
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-31 14:11:00 +0200

    Modified

--- NumeralsRoman/NumeralsRoman/UnitTest1.cs
+++ NumeralsRoman/NumeralsRoman/UnitTest1.cs
@@ -16,55 +16,22 @@ namespace NumeralsRoman
         {
             Assert.AreEqual("XXXV", ToRoman(35));
         }
-        string x;
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("XXXV", ToRoman(4500));
+        }
+        string result;
         string ToRoman(int number)
         {
-            int[ ] zecimalnumber = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 ,3000};
-            string[ ] romannumber = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M","MMM" };
-            int n=13;
-            if((number>=1)&&(number<=3999))
-                return "Empty";
-            else
-                {
-                 for(int i=zecimalnumber[n]; i>=0 ; i--)
+            int[ ] decimalNumber = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+            string[ ] romanNumber = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+            for (int i = decimalNumber.Length-1; i >= 0; i--)
                     {
-                         while(number>=zecimalnumber[i])
-                         {
-                             x=x +romannumber[i];
-                             number=number-zecimalnumber[i];
-                             n--;
-                         }
+                        for (; number >= decimalNumber[i]; number -= decimalNumber[i])
+                            result += romanNumber[i];
                      }
-                 }
-            return x;
-            
-
-
-
-
-
-
-
-        /*    while (number >0)
-            {
-                if ((number < 0) || (number > 3999)) return "Empty";
-                if (number >= 3000) { x = x + "MMM"; number = number - 3000; }
-                if (number >= 1000) { x = x + "M"; number = number - 1000; }
-                if (number >= 900) { x = x + "CM"; number = number - 900; }
-                if (number >= 500) { x = x + "D"; number = number - 500; }
-                if (number >= 400) { x = x + "CD"; number = number - 400; }
-                if (number == 100) { x = x + "C"; number = number - 100; }
-                if (number >= 90) { x = x + "XC"; number = number - 90; }
-                if (number >= 50) { x = x + "L"; number = number - 50; }
-                if (number >= 40) { x = x + "XL"; number = number - 40; }
-                if (number >= 20) { x = x + "XX"; number = number - 20; }
-                if (number >= 10) { x = x + "X"; number = number - 10; }
-                if (number >= 9) { x = x + "IX"; number = number - 9; }
-                if (number >= 5) { x = x + "V"; number = number - 5; }
-                if (number >= 4) { x = x + "IV"; number = number - 4; }
-                if (number >= 1) { x = x + "I"; number = number - 1; }
-            }
-            return x;*/
+            return result;
         }
 
     }
