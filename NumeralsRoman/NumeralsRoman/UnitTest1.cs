6daca01ea7624c34f83ade8253d416c76ffcc061
flaviu1 <tocaci.flaviu@yahoo.com>
2015-12-31 14:23:39 +0200

    FinalTestRomanNumerals

--- NumeralsRoman/NumeralsRoman/UnitTest1.cs
+++ NumeralsRoman/NumeralsRoman/UnitTest1.cs
@@ -19,18 +19,24 @@ namespace NumeralsRoman
         [TestMethod]
         public void TestForBiggerNumber3999()
         {
-            Assert.AreEqual("XXXV", ToRoman(4200));
+            Assert.AreEqual("Empty", ToRoman(4200));
         }
         string result;
         string ToRoman(int number)
         {
             int[ ] decimalNumber = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
             string[ ] romanNumber = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-            for (int i = decimalNumber.Length-1; i >= 0; i--)
-                    {
-                        for (; number >= decimalNumber[i]; number -= decimalNumber[i])
-                            result += romanNumber[i];
-                     }
+            if ((number >= 1) && (number <= 3999))
+            {
+                for (int i = decimalNumber.Length - 1; i >= 0; i--)
+                {
+                    for (; number >= decimalNumber[i]; number -= decimalNumber[i])
+                        result += romanNumber[i];
+                }
+            }
+            else
+                return "Empty";
+
             return result;
         }
 
