e8f539347a3d3973cda1a93889d46a76787860e8
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-05 00:14:09 +0200

    CreateFunctionPrefixAndTest

---
+++ Prefix/Prefix/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("s",CalculatePrefix("sa","sff"));
+        }
+        string CalculatePrefix(string stringA, string StringB )
+        {
+            return "s";
+        }
+
+     }
+}
