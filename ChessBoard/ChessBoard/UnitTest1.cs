46925715a471385b5b43c95f181c0906592140de
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-05 00:03:30 +0200

    TestChessBoard

---
+++ ChessBoard/ChessBoard/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessBoard
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(64, CalculateSquare(8));
+        }
+        int CalculateSquare(int n )
+        {
+            return n * n;
+        }
+    }
+}
