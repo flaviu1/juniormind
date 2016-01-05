1cb2be42e45a6dbebfa21d3b581a6c7b56b11fbb
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-05 18:39:44 +0200

    Panagram.

--- Panagram/Panagram/UnitTest1.cs
+++ Panagram/Panagram/UnitTest1.cs
@@ -9,25 +9,31 @@ namespace Panagram
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("it's a panagram", SearchForPanagram("The quick brown fox jumps over the lazy dog"));
+            Assert.AreEqual("it is a panagram", SearchForPanagram("the quick brown fox jumps over the lazy dog"));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("it is not panagram", SearchForPanagram("sada"));
         }
 
         string SearchForPanagram(string phrase )
         {
-            int contor = 0;
-         string[] alphabet={"abcdefghijklmnopqrstuvwxyz"};
+         int contor =0;
+         string alphabet="abcdefghijklmnopqrstuvwxyz";
          for (int i = 0; i <= alphabet.Length - 1; i++)
          {
              for (int j = 0; j <= phrase.Length - 1; j++)
              {
                  if (alphabet[i] == phrase[j])
                  {
-                     contor=1;
+                     contor+=1;
+                     break;
                  }
              }
              if (contor != 1) break;
          }
-         if (contor == 1) return "it's a panagram";
+         if (contor == 1) return "it is a panagram";
          else return "it is not panagram";
         }
     }
