33ff2db21bb406f5c1043b7257ab0568465e755e
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-05 12:51:35 +0200

    TestChessBoard

--- ChessBoard/ChessBoard/UnitTest1.cs
+++ ChessBoard/ChessBoard/UnitTest1.cs
@@ -9,11 +9,23 @@ namespace ChessBoard
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(64, CalculateSquare(8));
+            Assert.AreEqual(85, CalculateSquare(8));
+        }
+        [TestMethod]
+        public void TestMethod2()  
+        {
+            Assert.AreEqual(21, CalculateSquare(4));
         }
         int CalculateSquare(int n )
         {
-            return n * n;
+            int contor=n*n;
+            n = n * n;
+         do
+          {
+              n = n / 4;
+              contor += n;
+          } while (n >= 1) ;
+         return contor;
         }
     }
 }
