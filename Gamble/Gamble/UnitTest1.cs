e87f13837503d75fc181b3bbc04fd84b1c9940b4
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-06 19:40:22 +0200

    CreateFunctionFactorial

---
+++ Gamble/Gamble/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gamble
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestForCategoryI()
+        {
+            Assert.AreEqual(8, CalculateProbability(1));
+        }
+        [TestMethod]
+        public void TestForFactorial()
+        {
+            Assert.AreEqual(40320, CalculateFactorial(8));
+        }
+        int CalculateProbability(int categori)
+        {
+            int probability=0;
+            return probability;
+        }
+
+        int CalculateCombinari(int items ,int submultimi)
+        {
+            return 1;
+        }
+        int CalculateFactorial(int number)
+        {
+            int result = 1;
+            for (int i = 1; i < number+1; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
--- Panagram/Panagram/UnitTest1.cs
+++ Panagram/Panagram/UnitTest1.cs
@@ -6,35 +6,32 @@ namespace Panagram
     [TestClass]
     public class UnitTest1
     {
+        private bool folse;
         [TestMethod]
         public void TestMethod1()
         {
             Assert.AreEqual("it is a panagram", SearchForPanagram("the quick brown fox jumps over the lazy dog"));
         }
-        [TestMethod]
-        public void TestMethod2()
-        {
-            Assert.AreEqual("it is not panagram", SearchForPanagram("sada"));
-        }
 
         string SearchForPanagram(string phrase )
         {
-         int contor =0;
-         string alphabet="abcdefghijklmnopqrstuvwxyz";
-         for (int i = 0; i <= alphabet.Length - 1; i++)
-         {
-             for (int j = 0; j <= phrase.Length - 1; j++)
-             {
-                 if (alphabet[i] == phrase[j])
-                 {
-                     contor+=1;
-                     break;
-                 }
-             }
-             if (contor != 1) break;
-         }
-         if (contor == 1) return "it is a panagram";
-         else return "it is not panagram";
+            for (int i = 0; i <= phrase.Length - 1; i++)
+            {
+               if(SearchPoint(phrase,phrase[i]));
+            }
+        }
+
+        Boolean SearchPoint(string alphabet ,char point)
+        {
+            int contor=0;
+            for (int i = 0; i <= alphabet.Length - 1; i++)
+            {
+                   if(alphabet[i]==point)
+                   {
+                       return true;
+                   }
+            }
+            return folse;
         }
     }
 }
