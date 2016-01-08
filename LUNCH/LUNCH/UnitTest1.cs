31b3e7cccf93a50d8fdb39eab9beeb2252971369
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-08 17:53:49 +0200

    TestFoarLunchCMMMC

--- LUNCH/LUNCH/UnitTest1.cs
+++ LUNCH/LUNCH/UnitTest1.cs
@@ -9,6 +9,19 @@ namespace LUNCH
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(12, CalculateCMMMC(4, 6));
+        }
+        int CalculateCMMMC(int fourDay, int sixDay )
+        {
+            int product = fourDay * sixDay;
+            while (fourDay != sixDay)
+            {
+                if (fourDay > sixDay)
+                    fourDay = fourDay - sixDay;
+                else
+                    sixDay = sixDay - fourDay;
+            }
+            return product = product / fourDay;
         }
     }
 }
