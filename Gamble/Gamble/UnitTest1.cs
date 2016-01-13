5fd83b1e45e2de30172e75a1c86623d568e5c94d
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-13 14:10:05 +0200

    FinalTestGamble

--- Gamble/Gamble/UnitTest1.cs
+++ Gamble/Gamble/UnitTest1.cs
@@ -9,20 +9,40 @@ namespace Gamble
         [TestMethod]
         public void TestForFactorial()
         {
-            Assert.AreEqual(608281864034267560872252163321295376887552831379210240000000000, CalculateFactorial(49));
+            Assert.AreEqual(608281864034267560872252163321295376887552831379210240000000000d, CalculateFactorial(49));
         }
         [TestMethod]
         public void TestForCombinations()
         {
             Assert.AreEqual(13983816, CalculateCombinations(49, 6));
         }
-       
-        int CalculateProbability(int categori)
+        [TestMethod]
+        public void Test()
         {
-            int probability=0;
-            return probability; 
+            Assert.AreEqual((double)0.00000007,(double)CalculateProbability(1));
         }
-
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual((double)0.00001845,(double)CalculateProbability(2));
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual((double)0.00096862,(double)CalculateProbability(3));
+        }
+        double CalculateProbability(int categori)
+        {
+            double probability = 0;
+            if(categori==1)
+                probability+=CalculateCombinations(6,6)*CalculateCombinations(43,0)/CalculateCombinations(49,6);
+            if (categori == 2)
+                probability += CalculateCombinations(6, 5)*CalculateCombinations(43,1)/CalculateCombinations(49,6);
+            if (categori == 3)
+                probability += CalculateCombinations(6, 4) * CalculateCombinations(43, 2) / CalculateCombinations(49, 6);
+            return probability;
+        }
+            
          double CalculateCombinations(int number1 ,int number2)
         {
             double combinations = 0;
@@ -32,7 +52,7 @@ namespace Gamble
 
         double CalculateFactorial(int number)
         {
-            int result = 1;
+            double result = 1;
             for (int i = 1; i <= number; i++)
             {
                 result = result * i;
