f6cfc47c7767c4bbb3fc5d94002678551955652c
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-14 22:35:01 +0200

    Anagrame

---
+++ NumberOfAnagrame/NumberOfAnagrame/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumberOfAnagrame
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, CalculateNumberOfAnagrame("ssaa"));
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(15, CalculateNumberOfAnagrame("qqqwe"));
+        }
+        [TestMethod]
+        int CalculateNumberOfAnagrame(string phrase)
+        {
+            int contor = 0;
+            int subunitary = 0;
+            int result = 0;
+            char character = ' ';
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (phrase[i] == character)
+                    continue;
+                for (int j = 0; j < phrase.Length; j++)
+                {
+                    if (phrase[i] == phrase[j])
+                        contor++;
+                    character = phrase[i];
+                }
+                subunitary += CalculateFactorial(contor);
+                contor = 0;
+            }
+            return result = CalculateFactorial(phrase.Length) / subunitary;
+        }
+
+        int CalculateFactorial(int number)
+        {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
