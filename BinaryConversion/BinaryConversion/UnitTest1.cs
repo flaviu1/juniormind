3ca98c60d7b6384b3fa8d678a6f86641224c3c47
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-25 23:34:13 +0200

    ConvertToBinary

---
+++ BinaryConversion/BinaryConversion/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace BinaryConversion
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(new byte[6] {1,1,0,0,0,1},ConvertBinary(49));
+        }
+
+        byte[] ConvertBinary(int number)
+        {
+            byte[] bytes = new byte[6];
+            byte n = 0;
+            while (number >= 1)
+            {
+                bytes[n] =(byte)(number % 2);
+                n++;
+                number =number / 2;
+            }
+            Array.Reverse(bytes);
+            return bytes;
+        }
+    }
+}
