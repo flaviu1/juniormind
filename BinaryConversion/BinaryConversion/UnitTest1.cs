172e1e100875b7af5c1d42319f6c9ec5b2a7dd50
flaviu1 <tocaci.flaviu@yahoo.com>
2016-01-26 20:22:31 +0200

    Convert

--- BinaryConversion/BinaryConversion/UnitTest1.cs
+++ BinaryConversion/BinaryConversion/UnitTest1.cs
@@ -10,52 +10,53 @@ namespace BinaryConversion
         [TestMethod]
         public void TestForConversion()
         {
-            CollectionAssert.AreEqual(new byte[6] {1,1,0,0,0,1},ConvertBinary(49));
+            CollectionAssert.AreEqual(new byte[6] { 1,1,0,0,0,1 }, ConvertBinary(49));
         }
         [TestMethod]
-        public void TestForNot()
+        public void TestForOperatorNot()
         {
             CollectionAssert.AreEqual(new byte[6] {0,0,1,1,1,0},NotOperatorsOnByte(49));
         }
+        [TestMethod]
+        public void TestForOperatorOr()
+        {
+            CollectionAssert.AreEqual(new byte[6] { 1,1,0,0,0,1 }, OrOperatorsOnByte(49, 49));
+        }
+
+
         byte[] ConvertBinary(int number)
         {
-            byte[] bytes = new byte[6];
-            byte n = 0;
-            
-            while (number >= 1)
-            {
-                bytes[n] =(byte)(number % 2);
-                n++;
-                number =number / 2;
-            }
+            byte[] bytes = ConvertBinary(ref number);
             Array.Reverse(bytes);
             return bytes;
         }
         byte[] NotOperatorsOnByte(int number)
         {
+            byte[] bytes = ConvertBinary(ref number);
+            byte n = (byte)bytes.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (bytes[i] != 1)
+                {
+                    bytes[i] = 1;
+                }
+                else
+                    bytes[i] = 0;
+            }
+            Array.Reverse(bytes);
+            return bytes;
+        }
+        private static byte[] ConvertBinary(ref int number)
+        {
             byte[] bytes = new byte[6];
             byte n = 0;
-            byte[] notbytes=new byte[6];
-            byte m=0;
             while (number >= 1)
             {
                 bytes[n] = (byte)(number % 2);
                 n++;
                 number = number / 2;
             }
-            for (int i = 0; i < n;i++ )
-            {
-                if(bytes[i]!=1)
-                {
-                    bytes[i] = 1;
-                }
-                else
-                    bytes[i]=0;
-
-            }
-                Array.Reverse(bytes);
             return bytes;
-
         }
     }
 }
