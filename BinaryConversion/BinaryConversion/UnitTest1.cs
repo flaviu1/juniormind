f5a697506f7f007562403834bfaa007370ee987c
flaviu1 <tocaci.flaviu@yahoo.com>
2016-02-03 12:06:26 +0200

    Changes.

--- BinaryConversion/BinaryConversion/UnitTest1.cs
+++ BinaryConversion/BinaryConversion/UnitTest1.cs
@@ -10,17 +10,17 @@ namespace BinaryConversion
         [TestMethod]
         public void TestForConversion()
         {
-            CollectionAssert.AreEqual(new byte[] { 1,1,0,0,0,1 }, ConvertBinary(49));
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 0, 0, 0, 1 }, ConvertBinary(49));
         }
         [TestMethod]
         public void TestForOperatorNot()
         {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 1, 1, 0 }, NotOperatorsOnByte( new byte[] {1,1,0,0,0,1} ));
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 1, 1, 0 }, NotOperatorsOnByte(new byte[] { 1, 1, 0, 0, 0, 1 }));
         }
         [TestMethod]
         public void TestForOperatorAnd()
         {
-            CollectionAssert.AreEqual(new byte[] {  0, 0, 0, 1 }, AndOperatorsOnByte(new byte[] { 0, 1, 0, 1 }, new byte[4] { 0, 0, 1, 1 }));
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 0, 1 }, AndOperatorsOnByte(new byte[] { 0, 1, 0, 1 }, new byte[4] { 0, 0, 1, 1 }));
         }
         [TestMethod]
         public void TestForOperatorOr()
@@ -32,16 +32,16 @@ namespace BinaryConversion
         {
             CollectionAssert.AreEqual(new byte[] { 0, 1, 1, 0 }, XorOperatorsOnByte(new byte[] { 0, 1, 0, 1 }, new byte[4] { 0, 0, 1, 1 }));
         }
-      /*  [TestMethod]
-        public void ShiftLeft()
-        {
-            CollectionAssert.AreEqual(new byte[] { 1,0,0,0 }, ShiftLeft(new byte[] { 0, 0, 0, 1 }, 3));
-            CollectionAssert.AreEqual(new byte[2] { 1, 1 }, ShiftLeft(new byte[4] { 1, 1, 1, 1 }, 2));
-            CollectionAssert.AreEqual(new byte[0] { }, ShiftLeft(new byte[4] { 1, 1, 1, 1 }, 4));
-        }*/
+        /*  [TestMethod]
+          public void ShiftLeft()
+          {
+              CollectionAssert.AreEqual(new byte[] { 1,0,0,0 }, ShiftLeft(new byte[] { 0, 0, 0, 1 }, 3));
+              CollectionAssert.AreEqual(new byte[2] { 1, 1 }, ShiftLeft(new byte[4] { 1, 1, 1, 1 }, 2));
+              CollectionAssert.AreEqual(new byte[0] { }, ShiftLeft(new byte[4] { 1, 1, 1, 1 }, 4));
+          }*/
 
 
-     
+
         byte[] ConvertBinary(int number)
         {
             byte[] bytes = ConvertNumber(number);
@@ -66,7 +66,7 @@ namespace BinaryConversion
             byte[] bytes = new byte[6];
             byte n = 0;
             while (number >= 1)
-            {                
+            {
                 bytes[n] = (byte)(number % 2);
                 n++;
                 number = number / 2;
@@ -74,59 +74,45 @@ namespace BinaryConversion
             return bytes;
         }
 
-        byte[] AndOperatorsOnByte(byte[] bytesOne,byte[] bytesTwo)
+        byte[] AndOperatorsOnByte(byte[] bytesOne, byte[] bytesTwo)
         {
-            byte[] bytesAnd = new byte[Math.Max(bytesOne.Length, bytesTwo.Length)];
-            return bytesAnd=VerifyConditions(bytesOne,bytesTwo,bytesAnd,"And");
+            return VerifyConditions(bytesOne, bytesTwo, "And");
         }
         byte[] OrOperatorsOnByte(byte[] bytesOne, byte[] bytesTwo)
         {
-            byte[] bytesOr = new byte[Math.Max(bytesOne.Length, bytesTwo.Length)];
-            return bytesOr = VerifyConditions(bytesOne, bytesTwo, bytesOr,"Or");
+            return VerifyConditions(bytesOne, bytesTwo, "Or");
         }
         byte[] XorOperatorsOnByte(byte[] bytesOne, byte[] bytesTwo)
         {
-            byte[] bytesXor = new byte[Math.Max(bytesOne.Length, bytesTwo.Length)];
-            return bytesXor = VerifyConditions(bytesOne, bytesTwo,bytesXor,"Xor");
+            return VerifyConditions(bytesOne, bytesTwo, "Xor");
         }
-        byte[] VerifyConditions(byte[] bytesOne, byte[] bytesTwo,byte[] bytesLenght, string operatorr)
+        byte[] VerifyConditions(byte[] bytesOne, byte[] bytesTwo, string operatorr)
         {
-            byte[] result = new byte[bytesLenght.Length];
+            byte[] result = new byte[Math.Max(bytesOne.Length, bytesTwo.Length)];
             for (int i = 0; i < result.Length; i++)
             {
-                if (operatorr == "And")
-                {
-                    if (bytesOne[i] == 1 && bytesTwo[i] == 1)
-                        result[i] = 1;
-                    else
-                        result[i] = 0;
-                }
-                if (operatorr =="Or")
-                {
-                    if (bytesOne[i] == 1 || bytesTwo[i] == 1)
-                        result[i] = 1;
-                    else
-                        result[i] = 0;
-                }
-                if (operatorr =="Xor")
-                {
-                    if (bytesOne[i] != bytesTwo[i])
-                        result[i] = 1;
-                    else
-                        result[i] = 0;
-                }
+                Condition(bytesOne, bytesTwo, operatorr, result, i);
             }
-
             return result;
         }
 
-      /*  byte[] ShiftLeft(byte[] bytesOne, int positions)
+        private static void Condition(byte[] bytesOne, byte[] bytesTwo, string operatorr, byte[] result, int i)
         {
-            byte[] bytes2 = new byte[bytesOne.Length];
-            for (int i = positions; i < bytesOne.Length; i++)
-            {
-                bytes2[i] = bytesOne[i];
-            }
-            return bytes2;*/
+            if (operatorr == "And")
+                result[i] = (bytesOne[i] == 1) && (bytesTwo[i] == 1) ? (byte)1 : (byte)0;
+            if (operatorr == "Or")
+                result[i] = (bytesOne[i] == 1) || (bytesTwo[i] == 1) ? (byte)1 : (byte)0;
+            if (operatorr == "Xor")
+                result[i] = bytesOne[i] != bytesTwo[i] ? (byte)1 : (byte)0;
         }
-    }   
+
+        /*  byte[] ShiftLeft(byte[] bytesOne, int positions)
+          {
+              byte[] bytes2 = new byte[bytesOne.Length];
+              for (int i = positions; i < bytesOne.Length; i++)
+              {
+                  bytes2[i] = bytesOne[i];
+              }
+              return bytes2;*/
+    }
+}
