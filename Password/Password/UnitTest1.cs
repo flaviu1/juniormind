8892bdc9debf82b3fcbdedc73c7e1204a3f30c9a
flaviu1 <tocaci.flaviu@yahoo.com>
2016-02-29 11:45:39 +0200

    AddSimbols

--- Password/Password/UnitTest1.cs
+++ Password/Password/UnitTest1.cs
@@ -14,35 +14,45 @@ namespace Password
         [TestMethod]
         public void WhatCountingUppercaseLetter()
         {
-            Assert.AreEqual(10, CounteNumbers(GeneratePassword(20, 10, 10)));
+            Assert.AreEqual(9, CounteNumbers(GeneratePassword(20, 10, 9)));
         }
         [TestMethod]
         public void WhatsDigitsContainsPassword()
         {
-            Assert.AreEqual(10, ContingUppercasePassowrd(GeneratePassword(20, 10)));
+            Assert.AreEqual(9, ContingUppercasePassowrd(GeneratePassword(20, 9)));
         }
-        string GeneratePassword(int number , int uppercase=0,int digits=0)
+        [TestMethod]
+        public void ManySymbolsContainsPassword()
+        {
+            Assert.AreEqual(10, ContingSymbols(GeneratePassword(20, 5,5,10)));
+        }
+
+        string GeneratePassword(int number, int uppercase = 0, int digits = 0, int symbols = 0)
         {
             string result = string.Empty;
             Random rnd = new Random();
-            result = ConditionsForPassword(number, uppercase,digits, rnd);
+            result = ConditionsForPassword(number, uppercase, digits, rnd, symbols);
             return result;
         }
 
-        private static string ConditionsForPassword(int number, int uppercase,int digits, Random rnd)
+        private static string ConditionsForPassword(int number, int uppercase, int digits, Random rnd, int symbols)
         {
             string result = string.Empty;
-            for (int i = 0; i < number - uppercase-digits; i++)
+            for (int i = 0; i < number - uppercase - digits - symbols; i++)
             {
                 result += (char)rnd.Next('a', 'z' + 1);
             }
-            for (int i = 0; i < uppercase-digits; i++)
+            for (int i = 0; i < uppercase; i++)
             {
                 result += (char)rnd.Next('A', 'Z' + 1);
             }
             for (int i = 0; i < digits; i++)
             {
-                result +=rnd.Next(0, 9);
+                result += rnd.Next(0, 9);
+            }
+            for (int i = 0; i < symbols; i++)
+            {
+                result += (char)rnd.Next(33, 47);
             }
 
             return result;
@@ -51,13 +61,13 @@ namespace Password
         [TestMethod]
         public void ManyDigitsContainsThePhrase()
         {
-            Assert.AreEqual(3,CounteNumbers("as113"));
+            Assert.AreEqual(3, CounteNumbers("as113"));
             Assert.AreEqual(5, CounteNumbers("a11s113"));
         }
         int CounteNumbers(string word)
         {
-            int counter=0;
-           foreach(char c in word)
+            int counter = 0;
+            foreach (char c in word)
             {
                 if (char.IsNumber(c))
                     counter++;
@@ -74,13 +84,59 @@ namespace Password
         int ContingUppercasePassowrd(string word)
         {
             int counter = 0;
-            foreach(char c in word)
+            foreach (char c in word)
             {
                 if (char.IsUpper(c))
                     counter++;
             }
             return counter;
         }
-    } 
+
+        [TestMethod]
+        public void HowManySymbolsContains()
+        {
+            Assert.AreEqual(5, ContingSymbols("*+-()asd"));
+        }
+        int ContingSymbols(string symbols)
+        {
+            int counter = 0;
+            for (int i = 0; i < symbols.Length; i++)
+                for (int j = 33; j <= 47; j++)
+                {
+                    if (symbols[i] == j)
+                    {
+                        counter++;
+                    }
+                }
+            return counter;
+        }
+
+
+        bool CheckIsNotAmbiguousCharacters(char character)
+        {
+            byte[] symbolsAmbigue = { 123, 125, 91, 93, 40, 41, 47, 92, 39, 34, 126, 44, 59, 46, 60, 62 };//numbers is ambiguous characters: "{}[]()/\'"~,;.<>".
+            for (int i = 0; i < symbolsAmbigue.Length;i++)
+               {
+                    if(character==(char)symbolsAmbigue[i])
+                    {
+                        return true;
+                    }
+               }
+                return false;
+        }
+
+        bool CheckForSimilarCharacters(char character)
+        {
+            byte[] symbolsSimilar = { 108, 49, 73, 111, 48, 79 };//numbers is Similar characters: " l, 1, I, o, 0, O";.
+            for (int i = 0; i < symbolsSimilar.Length; i++)
+            {
+                if (character == (char)symbolsSimilar[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
 
