4bb295f5618649f919cf0d204be96440e084d27b
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-03 21:47:59 +0200

    ChangeAddProduct

--- Shoping/Shoping/UnitTest1.cs
+++ Shoping/Shoping/UnitTest1.cs
@@ -21,11 +21,10 @@ namespace Shoping
         public void TheSumOfProducts()
         {
             var product = new Product[] { new Product("Apple", 10), new Product("Sugar", 15), new Product("Orange", 15) };
-            Assert.AreEqual(40, CalculatePriceShopping(product));
+            Assert.AreEqual(40, CalculateTheSumOfProducts(product));
         }
 
-
-        double CalculatePriceShopping(Product[] product)
+        double CalculateTheSumOfProducts(Product[] product)
         {
             double result = 0;
             for (int i = 0; i < product.Length;i++)
@@ -39,12 +38,17 @@ namespace Shoping
         {
             var product = new Product[] { new Product("Apple", 10), new Product("Sugar", 25), new Product("Orange", 15) };
             Assert.AreEqual(10, TheCheapestProduct(product));
+            var productEmpty= new Product[] { };
+            Assert.AreEqual(0, TheCheapestProduct(productEmpty));
         }
-        double TheCheapestProduct(Product[] product)
+        double TheCheapestProduct(Product[] product)    
         {
-            double result =1000;
-            for (int i = 0; i < product.Length; i++)
+            double result = 0;
+            for (int i = 1; i < product.Length; i++)
             {
+                if (product[0].priceProduct == 0)
+                    return 0;
+                result = product[0].priceProduct;
                 if (result > product[i].priceProduct)
                 {
                     result = product[i].priceProduct;
@@ -53,35 +57,57 @@ namespace Shoping
             return result;
         }
 
-        [TestMethod]
-        public void ProdusuCheapest1()
+       [TestMethod]
+        public void AddProduct()
         {
            var product = new Product[] { new Product("Apple",10), new Product("Sugar", 25), new Product("Orange", 15) };
-           var first = new Product[] { new Product("Apple", 10), new Product("Orange", 15) };
-           Assert.AreEqual(first, ToEliminateTheCheapestProduct(product));
+           var first = new Product[] { new Product("Apple", 10),new Product("Sugar", 25), new Product("Coffe", 20), new Product("Orange", 15) };
+           CollectionAssert.AreEqual(first, AddAProduct(product , 2 , "Coffe" , 20));
         }
-        Product[] ToEliminateTheCheapestProduct(Product[] product)
+        Product[] AddAProduct(Product[] product,int pozition,string name , int price)
+        {
+            Array.Resize(ref product, product.Length+1);
+            for (int i = pozition; i < product.Length+1; i++)
+            {
+                if (i + 1 < product.Length )
+                {
+                    product[i + 1].priceProduct = product[i].priceProduct;
+                    product[i + 1].nameProduct = product[i].nameProduct;
+                }
+            }
+            product[pozition].priceProduct = price;
+            product[pozition].nameProduct = name;
+            return product;
+        }
+        
+        [TestMethod]
+        public void RemoveAProduct ()
+        {
+            var product = new Product[] { new Product("Apple", 10), new Product("Sugar", 25), new Product("Orange", 15) };
+            var first = new Product[] { new Product("Apple", 10), new Product("Orange", 15) };
+            CollectionAssert.AreEqual(first, RemoveAProduct(product));
+        }
+        Product[] RemoveAProduct(Product[] product)
         {
             double counter = 0;
-            int index=0;
+            int index = 0;
             for (int i = 0; i < product.Length; i++)
             {
                 if (counter < product[i].priceProduct)
                 {
-                    counter = product[i].priceProduct;
-                    index=i;
+                    counter = product[i].priceProduct; 
+                    index = i;
                 }
             }
-            for (int i = index; i < product.Length-index;i++)
+            for (int i = index; i < product.Length - index; i++)
             {
-                product[i].priceProduct = product[i+1].priceProduct;
+                product[i].priceProduct = product[i + 1].priceProduct;
                 product[i].nameProduct = product[i + 1].nameProduct;
             }
             Array.Resize(ref product, product.Length - 1);
             return product;
         }
-
-
+         
         [TestMethod]
         public void ShouldCalculateTheAveragePrice()
         {
