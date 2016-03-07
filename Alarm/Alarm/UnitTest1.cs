f3afc1a98434e20736ab2f874fd9b6cf1ceae3cc
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-07 13:07:33 +0200

    Revert "Ciclometru"
    This reverts commit f82c279bc9854ad90362c47b9eb12c52ce9efe4a.

--- Alarm/Alarm/UnitTest1.cs
+++ Alarm/Alarm/UnitTest1.cs
@@ -3,31 +3,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Alarm
 {
-    enum Days { Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday };
-
-    [TestClass]
-    public class UnitTest1 
+    enum alarm
     {
-      // enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+
+    }
+    [TestClass]
+    public class UnitTest1
+    {
         [TestMethod]
         public void TestMethod1()
         {
-            int[] alarm={(int)Days.Monday,(int)Days.Tuesday, (int)Days.Wednesday,(int)Days.Thursday,(int)Days.Friday,(int)Days.Saturday, (int)Days.Sunday};
-           // Assert.AreEqual("yes",TriggeredAlarm((int)Days.Monday, 9));
-            Assert.AreEqual("yes", TriggeredAlarm(alarm,8));
-        }
 
-            
-       string TriggeredAlarm(int[] days ,int hour)
-       {
-           int counter=days.Length;
-           for(int i=0;i<days.legth;i++)
-           {
-               if()
-           }
-         
-           //return "yes";
-       }
+        }
 
     }
 }
