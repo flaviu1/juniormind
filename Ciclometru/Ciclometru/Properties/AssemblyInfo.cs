3fb67846089f88f74b9306bc65a4c43f0dec4fa0
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-07 13:11:00 +0200

    Ciclometru

--- Ciclometru/Ciclometru/Properties/AssemblyInfo.cs
+++ Ciclometru/Ciclometru/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alarm")]
+[assembly: AssemblyTitle("Ciclometru")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Alarm")]
+[assembly: AssemblyProduct("Ciclometru")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62b3ce59-0167-4290-9f36-e87feb364be4")]
+[assembly: Guid("eca793fe-0da8-49ab-b3f6-e0a52305df16")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Ciclometru/Ciclometru/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciclometru
+{
+    public struct Ciclometru
+    {
+        public string nameCiclometru;
+        public double rotationCiclometru;
+        public double diameterCiclometru;
+        public Ciclometru(string nameCiclometru, double rotationCiclometru, double diameterCiclometru)
+        {
+            this.nameCiclometru = nameCiclometru;
+            this.rotationCiclometru = rotationCiclometru;
+            this.diameterCiclometru = diameterCiclometru;
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DistanceTraveledByCyclists()
+        {
+            var distance = new Ciclometru[] { new Ciclometru("Alin", 1000, 10), new Ciclometru("Bogdan", 1100, 11), new Ciclometru("Silvian", 1200, 12) };
+            Assert.AreEqual(300, DistanceTraveled(distance));
+        }
+        double DistanceTraveled(Ciclometru[] distance)
+        {
+            double result = 0;
+            for (int i = 0; i < distance.Length; i++)
+            {
+                result += DistanceTraveledByACyclist(distance, i);
+            }
+            return result;
+        }
+
+        private static double DistanceTraveledByACyclist(Ciclometru[] distance, int i)
+        {
+            double result = 0;
+            result = (distance[i].rotationCiclometru / distance[i].diameterCiclometru);
+            return result;
+        }
+
+        [TestMethod]
+        public void AverageDrivingSpeed()
+        {
+            var distance = new Ciclometru[] { new Ciclometru("Alin", 1000, 10), new Ciclometru("Bogdan", 1100, 10), new Ciclometru("Silvian", 1200, 10) };
+            Assert.AreEqual("Alin", AverageDrivingSpeed(distance));
+        }
+        string AverageDrivingSpeed(Ciclometru[] distance)
+        {
+            double result = 0;
+            double counter = 0;
+            int index = 0;
+            for (int i = 0; i < distance.Length; i++)
+            {
+                counter = (DistanceTraveledByACyclist(distance, i)) / (distance[i].rotationCiclometru / 60);
+                if (counter > result)
+                {
+                    result = counter;
+                    index = i;
+                }
+            }
+            return distance[index].nameCiclometru;
+        }
+    }
+}
