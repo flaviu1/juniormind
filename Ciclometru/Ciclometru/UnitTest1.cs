943133d4c4e9746f6d8084e16ac7e89a07a81ecb
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-10 23:04:36 +0200

    Requirement_1

--- Ciclometru/Ciclometru/UnitTest1.cs
+++ Ciclometru/Ciclometru/UnitTest1.cs
@@ -2,18 +2,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 
+
 namespace Ciclometru
 {
-    public struct Bicyclist
+    public struct Cyclist
     {
         public string name;
         public int[] records;
         public int diameter;
-        public Bicyclist(string nameBicyclist, int[] recordsBicyclist, int diameterBicyclist)
+        public Cyclist(string name, int[] records, int diameter)
         {
-            this.name = nameBicyclist;
-            this.records = recordsBicyclist;
-            this.diameter = diameterBicyclist;
+            this.name = name;
+            this.records = records;
+            this.diameter = diameter;
         }
     }
 
@@ -23,89 +24,29 @@ namespace Ciclometru
         [TestMethod]
         public void DistanceTraveledByCyclists()
         {
-            var bicyclists = new Bicyclist[] { new Bicyclist("Alin", new int[] { 1, 2, 3 }, 3), new Bicyclist("Bogdan", new int[] { 2, 2, 4 }, 4), new Bicyclist("Silvian", new int[] { 2, 3, 5 }, 5) };
-            Assert.AreEqual(6, DistanceTraveled(bicyclists));
+            var cyclist = new Cyclist[] { new Cyclist("Alin", new int[] { 1, 2, 3 }, 3), new Cyclist("Bogdan", new int[] { 1, 2, 4 }, 3), new Cyclist("Silvian", new int[] { 1, 3, 6 }, 3) };
+            Assert.AreEqual(216, DistanceTraveled(cyclist));
         }
-        int DistanceTraveled(Bicyclist[] bicyclists)
+        int DistanceTraveled(Cyclist[] cyclists)
         {
-            int result = 0;
-            foreach (Bicyclist currentBicyclist in bicyclists)
+            double result = 0;
+            for ( int i = 0; i < cyclists.Length;i++)
             {
-                result += DistanceOneBicyclists(currentBicyclist);
+                int sum = 0;
+                sum = Sum(cyclists, i);
+                result += Math.PI * sum * cyclists[i].diameter;
             }
-            return result;
+                return (int)result;
         }
 
-
-        [TestMethod]
-         public void DistanceOneBicyclists()
-         {
-             var bicyclists = new Bicyclist[] { new Bicyclist("Alin", new int[] { 1, 2, 3 }, 3)};
-             Assert.AreEqual(2, DistanceOneBicyclists( new Bicyclist("Alin", new int[] { 1, 2, 3 }, 3)));
-         }
-         private static int DistanceOneBicyclists( Bicyclist currentBicyclist)
-         {
-             int result = 0;
-             int totalDistance = 0;
-             totalDistance = currentBicyclist.records.Sum();
-             result += totalDistance / currentBicyclist.diameter;
-             return result;
-         }
-
-
-         [TestMethod]
-         public void RiderWithAverageSpeed()
-         {
-             var bicyclists = new Bicyclist[] { new Bicyclist("Alin", new int[] { 3, 3, 3 }, 2), new Bicyclist("Bogdan", new int[] { 4, 4, 4 }, 2), new Bicyclist("Silvian", new int[] { 6, 6, 6 }, 2) };
-             Assert.AreEqual("Silvian", RiderWithAverageSpeed(bicyclists));
-             var bicyclists1 = new Bicyclist[] { new Bicyclist("Silvian", new int[] { 6, 6, 6 }, 2) , new Bicyclist("Alin", new int[] { 3, 3, 3 }, 3), new Bicyclist("Bogdan", new int[] { 4, 4, 4 }, 4)};
-             Assert.AreEqual("Silvian", RiderWithAverageSpeed(bicyclists1));
-         }
-        string RiderWithAverageSpeed(Bicyclist[] bicyclists)
-         {
-             int speed = 0;
-            int counter=0;
-            string result =string.Empty;
-            string cursier=string.Empty;
-            int AverageSpeed=0;
-            for(int i=0; i<bicyclists.Length; i++)
-            {
-                foreach (Bicyclist time in bicyclists)
-                {
-                    counter++;
-                }
-                cursier = bicyclists[i].name;
-                AverageSpeed = DistanceOneBicyclists(bicyclists[i]) / counter;
-                if (speed < AverageSpeed) 
-                {
-                    speed = AverageSpeed;
-                    result = cursier;
-                }
-                counter = 0;
-            }
-            return result;
-         }
-
-
-         [TestMethod]
-        public void CyclistWithMaximumSpeed()
-         {
-             var bicyclists = new Bicyclist[] { new Bicyclist("Alin", new int[] { 3, 3, 3 }, 2), new Bicyclist("Bogdan", new int[] { 4, 4, 4 }, 2), new Bicyclist("Silvian", new int[] { 6, 6, 6 }, 2) };
-             Assert.AreEqual("Silvian", CyclistWithMaximumSpeed(bicyclists));
-         }
-        string CyclistWithMaximumSpeed(Bicyclist[] bicyclists)
+        private static int Sum(Cyclist[] cyclists, int i)
         {
-            int second = 0;
-            int[] counter = { 0 };
-            for (int i = 0; i < bicyclists.Length;i++ )
+            int sum=0;
+            for (int j = 0; j < cyclists[i].records.Length; j++)
             {
-                foreach (Bicyclist currentBicyclist in bicyclists)
-                {
-                    counter = currentBicyclist.records;
-                    if()
-                }
+                sum += cyclists[i].records[j];
             }
-            return " a";
+            return sum;
         }
     }
 }
