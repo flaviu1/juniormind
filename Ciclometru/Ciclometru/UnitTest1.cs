7b874978cc7cc3bf849016350783daa9491bc1cb
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-11 10:55:09 +0200

    AddFunctionAndTeste

--- Ciclometru/Ciclometru/UnitTest1.cs
+++ Ciclometru/Ciclometru/UnitTest1.cs
@@ -32,11 +32,16 @@ namespace Ciclometru
             double result = 0;
             for ( int i = 0; i < cyclists.Length;i++)
             {
-                int sum = 0;
-                sum = Sum(cyclists[i], i);
-                result += Math.PI * sum * cyclists[i].diameter;
+               result +=DistancetTraveledByACyclist(cyclists, i);
             }
                 return (int)result;
+        }
+
+        private static double DistancetTraveledByACyclist(Cyclist[] cyclists, int i)
+        {
+            int sum = 0;
+            sum = TheAmountOfRotationsOfACyclist(cyclists[i]);
+            return Math.PI * sum * cyclists[i].diameter;
         }
 
 
@@ -45,9 +50,9 @@ namespace Ciclometru
         {
             var cyclist = new Cyclist("Alin", new int[] { 1, 2, 3 }, 3);
             int i = cyclist.records.Length;
-            Assert.AreEqual(6, Sum(cyclist,i));
+            Assert.AreEqual(6, TheAmountOfRotationsOfACyclist(cyclist));
         }
-        private static int Sum(Cyclist cyclists, int i)
+        private static int TheAmountOfRotationsOfACyclist(Cyclist cyclists)
         {
             int sum=0;
             for (int j = 0; j < cyclists.records.Length; j++)
@@ -70,12 +75,10 @@ namespace Ciclometru
             double result = 0;
             double counter=0;
             int index = 0;
-            int seconds = 0;
             for (int i = 0; i < cyclist.Length; i++)
             {
                 counter = 0;
-                seconds = cyclist[i].records.Length;
-                counter += Sum(cyclist[i], i)/seconds;
+               counter+=AverageSpeedForOneCyclist(cyclist, i);
                 if(result<counter)
                 {
                     result = counter;
@@ -85,31 +88,51 @@ namespace Ciclometru
                 return cyclist[index].name;
         }
 
+        private static double AverageSpeedForOneCyclist(Cyclist[] cyclist, int i)
+        {
+            int seconds = 0;
+            seconds = cyclist[i].records.Length;
+            return TheAmountOfRotationsOfACyclist(cyclist[i]) / seconds;
+        }
+
 
         [TestMethod]
         public void CyclistWithSpeedMaximum()
         {
-            var cyclist = new Cyclist[] { new Cyclist("Alin", new int[] { 1, 2, 9 }, 3), new Cyclist("Bogdan", new int[] { 1, 2, 4 }, 3), new Cyclist("Silvian", new int[] { 1, 3, 6 }, 3) };
-            Assert.AreEqual("Alin", CyclistWithSpeedMaximum(cyclist));
+            var cyclist = new Cyclist[] { new Cyclist("Alin", new int[] { 1, 2, 3 }, 3), new Cyclist("Bogdan", new int[] { 1, 2, 4 }, 3), new Cyclist("Silvian", new int[] { 1, 3, 6 }, 3) };
+            Assert.AreEqual("Silvian", CyclistWithSpeedMaximum(cyclist));
         }
         string CyclistWithSpeedMaximum(Cyclist[] cyclist)
         {
            int index1 = 0;
            double result = 0;
-           int counter=0;
+           double speed=0;
            for(int i=0;i<cyclist.Length;i++)
            {
-               for (int j = 0; j < cyclist[i].records.Length;j++)
+               speed=SpeedForOneCyclist(cyclist, i);
+               if(result<speed)
                {
-                   counter=cyclist[i].records[j];
-                   if (result < counter)
-                   {
-                       result = counter;
-                       index1 = i;
-                   }
+                   result = speed;
+                   index1 = i;
                }
            }
            return cyclist[index1].name;
+        }
+
+
+        [TestMethod]
+        public void SpeedOneCyclist()
+        {
+            var cyclist = new Cyclist[] { new Cyclist("Alin", new int[] { 1, 2, 3 }, 3)};
+            Assert.AreEqual(2,2, SpeedForOneCyclist(cyclist,0));
+        }
+        private static double SpeedForOneCyclist(Cyclist[] cyclist, int i)
+        {
+            double distance = 0;
+            int time = 0;
+            distance = DistancetTraveledByACyclist(cyclist, i);
+            time = cyclist[i].records.Length;
+            return distance * time / 60;
         }
     }
 }
