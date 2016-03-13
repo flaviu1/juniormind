056c774eb90f56bbfde3bf5cc631c6c54c467c94
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-13 23:50:17 +0200

    ChangeFunctionAndMaximum

--- Ciclometru/Ciclometru/UnitTest1.cs
+++ Ciclometru/Ciclometru/UnitTest1.cs
@@ -46,16 +46,16 @@ namespace Ciclometru
             double result = 0;
             for ( int i = 0; i < cyclists.Length;i++)
             {
-               result +=DistancetTraveledByACyclist(cyclists, i);
+               result +=DistancetTraveledByACyclist(cyclists[i]);
             }
                 return (int)result;
         }
 
-        private static double DistancetTraveledByACyclist(Cyclist[] cyclists, int i)
+        private static double DistancetTraveledByACyclist(Cyclist cyclists)
         {
             int sum = 0;
-            sum = TheAmountOfRotationsOfACyclist(cyclists[i]);
-            return Math.PI * sum * cyclists[i].diameter;
+            sum = TheAmountOfRotationsOfACyclist(cyclists);
+            return Math.PI * sum * cyclists.diameter;
         }
 
 
@@ -92,7 +92,7 @@ namespace Ciclometru
             for (int i = 0; i < cyclist.Length; i++)
             {
                 counter = 0;
-               counter+=AverageSpeedForOneCyclist(cyclist, i);
+               counter =AverageSpeedForOneCyclist(cyclist[i]);
                 if(result<counter)
                 {
                     result = counter;
@@ -102,11 +102,11 @@ namespace Ciclometru
                 return cyclist[index].name;
         }
 
-        private static double AverageSpeedForOneCyclist(Cyclist[] cyclist, int i)
+        private static double AverageSpeedForOneCyclist(Cyclist cyclist)
         {
             int seconds = 0;
-            seconds = cyclist[i].records.Length;
-            return TheAmountOfRotationsOfACyclist(cyclist[i]) / seconds;
+            seconds = cyclist.records.Length;
+            return TheAmountOfRotationsOfACyclist(cyclist) / seconds;
         }
 
 
@@ -119,37 +119,47 @@ namespace Ciclometru
         }
         Maximum CyclistWithSpeedMaximum(Cyclist[] cyclist)
         {
-           int index1 = 0;
+           int index = 0;
            Maximum[] maximum = new Maximum[1];
            double result = 0;
            double speed=0;
            for(int i=0;i<cyclist.Length;i++)
            {
-               speed=SpeedForOneCyclist(cyclist, i);
+               speed=SpeedForOneCyclist(cyclist[i]);
                if(result<speed)
                {
                    int second = 0;
                     result = speed;
-                    index1 = i;
-                    maximum[0].name = cyclist[i].name;
-                    second+=SecondWhitMaximumSpeed(cyclist, index1);
-                    maximum[0].second = second;
-                    maximum[0].diameter = cyclist[i].diameter;
+                    index=i;
+                    second+=SecondWhitMaximumSpeed(cyclist[index]);
+                    AddToCyclistWhitSpeedMaximumNewStruct(cyclist, index, maximum);
                }
            }
            return maximum[0];
         }
 
-        private static int SecondWhitMaximumSpeed(Cyclist[] cyclist, int index1)
+        private static void AddToCyclistWhitSpeedMaximumNewStruct(Cyclist[] cyclist, int index, Maximum[] maximum)
+        {
+            for (int j = 0; j < maximum.Length; j++)
+            {
+                int second = 0;
+                second += SecondWhitMaximumSpeed(cyclist[index]);
+                maximum[j].name = cyclist[index].name;
+                maximum[j].second = second;
+                maximum[j].diameter = cyclist[index].diameter;
+            }
+        }
+
+        private static int SecondWhitMaximumSpeed(Cyclist cyclist)
         {
             int counter = 0;
             int second=0;
-            for (int j = 0; j < cyclist[index1].records.Length; j++)
+            for (int j = 0; j < cyclist.records.Length; j++)
             {
                 int count = 0;
-                if (count < cyclist[index1].records[j])
+                if (count < cyclist.records[j])
                 {
-                    counter = cyclist[index1].records[j];
+                    counter = cyclist.records[j];
                     second = j;
                 }
             }
@@ -160,15 +170,15 @@ namespace Ciclometru
         [TestMethod]
         public void SpeedOneCyclist()
         {
-            var cyclist = new Cyclist[] { new Cyclist("Alin", new int[] { 1, 2, 3 }, 3)};
-            Assert.AreEqual(2,2, SpeedForOneCyclist(cyclist,0));
+            var cyclist =  new Cyclist("Alin", new int[] { 1, 2, 3 }, 3);
+            Assert.AreEqual(2,2, SpeedForOneCyclist(cyclist));
         }
-        private static double SpeedForOneCyclist(Cyclist[] cyclist, int i)
+        private static double SpeedForOneCyclist(Cyclist cyclist)
         {
             double distance = 0;
             int time = 0;
-            distance = DistancetTraveledByACyclist(cyclist, i);
-            time = cyclist[i].records.Length;
+            distance = DistancetTraveledByACyclist(cyclist);
+            time = cyclist.records.Length;
             return distance * time / 60;
         }
     }
