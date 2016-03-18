c69dd7722ae72029593a7e9a2f1bb163b19a76ca
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-18 11:06:02 +0200

    AddFlags

--- Alarm/Alarm/UnitTest1.cs
+++ Alarm/Alarm/UnitTest1.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    [TestClass]
-    public class UnitTest1 
+[TestClass]
+public class UnitTest1
+{
+    [Flags]
+    public enum Days
     {
-      public enum Days { 
-           Monday,
-           Tuesday,
-           Wednesday,
-           Thursday,
-           Friday,
-           Saturday, 
-           Sunday
-       }
-        struct Alarm
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8,
+        Friday = 16,
+        Saturday = 32,
+        Sunday = 64
+    }
+    struct Alarm
+    {
+        public Days day;
+        public int hour;
+        public Alarm(Days day, int hour)
         {
-            public Days day;
-            public int hour;
-            public Alarm(Days day, int hour)
-            {
-                this.day=day;
-                this.hour=hour;
-            }
+            this.day = day;
+            this.hour = hour;
         }
-        [TestMethod]
-        public void TestMethod1()
-        {
-           var alarm = new Alarm[] {   new Alarm (Days.Monday,6),
+    }
+
+    [TestMethod]
+    public void CheckAlarm()
+    {
+        var alarm = new Alarm[] {   new Alarm (Days.Monday,6),
                                        new Alarm (Days.Tuesday,6),
                                        new Alarm (Days.Wednesday,6),
                                        new Alarm (Days.Thursday,6),
@@ -34,22 +37,31 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
                                        new Alarm (Days.Saturday,8),
                                        new Alarm (Days.Sunday,8),
            };
-            Assert.IsTrue(TriggeredAlarm(alarm,Days.Saturday,8));
-            Assert.IsFalse(TriggeredAlarm(alarm, Days.Saturday, 6));
-            Assert.IsTrue(TriggeredAlarm(alarm, Days.Tuesday, 6));
-            Assert.IsFalse(TriggeredAlarm(alarm, Days.Tuesday, 8));
-
-
-        }
- 
-       bool TriggeredAlarm(Alarm[] alarm,Days day ,int hour)
-       {
-           for(int i=0;i<alarm.Length;i++)
-           {
-               if(alarm[i].day ==day && alarm[i].hour == hour)
-                   return true;
-           }
-            return false;
-       }
-
+        Assert.IsTrue(TriggeredAlarm(alarm, Days.Saturday, 8));
+        Assert.IsFalse(TriggeredAlarm(alarm, Days.Saturday, 6));
+        Assert.IsTrue(TriggeredAlarm(alarm, Days.Tuesday, 6));
+        Assert.IsFalse(TriggeredAlarm(alarm, Days.Tuesday, 8));
     }
+    [TestMethod]
+    public void CheckAlarmWithFlags()
+    {
+            var alarm = new Alarm[] {  
+                new Alarm (Days.Monday | Days.Tuesday | Days.Wednesday | Days.Thursday | Days.Friday,6),
+                new Alarm (Days.Saturday | Days.Sunday, 8 )
+               };
+        Assert.IsTrue(TriggeredAlarm(alarm, Days.Saturday, 8));
+        Assert.IsTrue(TriggeredAlarm(alarm, Days.Tuesday, 6));
+        Assert.IsFalse(TriggeredAlarm(alarm, Days.Saturday, 6));
+        Assert.IsFalse(TriggeredAlarm(alarm, Days.Tuesday, 8));
+    }
+
+    bool TriggeredAlarm(Alarm[] alarm, Days day, int hour)
+    {
+        for (int i = 0; i < alarm.Length; i++)
+        {
+            if ((alarm[i].day & day)!=0  && (alarm[i].hour == hour))
+                return true;
+        }
+        return false;
+    }
+}
