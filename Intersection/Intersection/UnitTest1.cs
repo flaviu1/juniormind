6f133bbc1155b9267cccab3f9475b88f954acb8b
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-18 12:45:28 +0200

    TronsoaneIntersection

---
+++ Intersection/Intersection/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Intersection
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public enum Direction
+        {
+            up,
+            down,
+            right,
+            left
+        }
+        public struct PointIntersection
+        {
+            public int x;
+            public int y;
+            public PointIntersection(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Direction[] direction = new Direction[] {
+                Direction.right,
+                Direction.up,
+                Direction.up,
+                Direction.right,
+                Direction.down,
+                Direction.left
+            };
+            var pointIntersection = new PointIntersection(1, 1);
+            Assert.AreEqual(pointIntersection, CheckIfIntersection(direction));
+        }
+
+        PointIntersection CheckIfIntersection(Direction[] direction)
+        {
+            int cordx = 0;
+            int cordy = 0;
+            PointIntersection intersection = new PointIntersection(0, 0);
+            for (int i = 0; i < direction.Length; i++)
+            {
+                if (direction[i] == Direction.up) cordy++;
+                if (direction[i] == Direction.down) cordy--;
+                if (direction[i] == Direction.right) cordx++;
+                if (direction[i] == Direction.left) cordx--;
+            }
+            intersection.x = cordx;
+            intersection.y = cordy;
+            return intersection;
+        }
+    }
+}
