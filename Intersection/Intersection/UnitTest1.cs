f17a3c42cbf0746c046981db04be9509075bbd83
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-18 23:07:18 +0200

    EditPointAddTest

--- Intersection/Intersection/UnitTest1.cs
+++ Intersection/Intersection/UnitTest1.cs
@@ -13,11 +13,11 @@ namespace Intersection
             right,
             left
         }
-        public struct PointIntersection
+        public struct Point
         {
             public int x;
             public int y;
-            public PointIntersection(int x, int y)
+            public Point(int x, int y)
             {
                 this.x = x;
                 this.y = y;
@@ -34,24 +34,44 @@ namespace Intersection
                 Direction.down,
                 Direction.left
             };
-            var pointIntersection = new PointIntersection(1, 1);
+            var pointIntersection = new Point(1, 1);
             Assert.AreEqual(pointIntersection, CheckIfIntersection(direction));
         }
-
-        PointIntersection CheckIfIntersection(Direction[] direction)
+        [TestMethod]
+        public void CeckPoint()
         {
-            int cordx = 0;
-            int cordy = 0;
-            PointIntersection intersection = new PointIntersection(0, 0);
+            Direction[] direction = new Direction[] {
+                Direction.right,
+                Direction.up,
+                Direction.up,
+                Direction.right,
+                Direction.down,
+                Direction.left,
+                 Direction.left,
+                 Direction.left
+            };
+            var pointIntersection = new Point(1, 1);
+            Assert.AreEqual(pointIntersection, CheckIfIntersection(direction));
+        }
+        Point CheckIfIntersection(Direction[] direction)
+        {
+            Point intersection = new Point(0, 0);
+            Point[] intersection2 = new Point[direction.Length];
             for (int i = 0; i < direction.Length; i++)
             {
-                if (direction[i] == Direction.up) cordy++;
-                if (direction[i] == Direction.down) cordy--;
-                if (direction[i] == Direction.right) cordx++;
-                if (direction[i] == Direction.left) cordx--;
+                if (direction[i] == Direction.up) intersection.y +=1 ;
+                if (direction[i] == Direction.down) intersection.y -= 1;
+                if (direction[i] == Direction.right) intersection.x += 1;
+                if (direction[i] == Direction.left) intersection.x -= 1;
+
+                for(int j=0;j<intersection2.Length;j++)
+                {
+                    if (intersection2[j].x == intersection.x && intersection2[j].y == intersection.y)
+                        return intersection;
+                }
+                intersection2[i].x = intersection.x;
+                intersection2[i].y = intersection.y;
             }
-            intersection.x = cordx;
-            intersection.y = cordy;
             return intersection;
         }
     }
