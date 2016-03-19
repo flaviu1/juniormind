4cd5ca5fcec78b4885f0ad08d6f226db10e67171
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-19 16:16:10 +0200

    Fibonacci

---
+++ Fibonacci/Fibonacci/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(13,Fibonacci(7));
+        }
+        [TestMethod]
+        public void FibonacciOf15()
+        {
+            Assert.AreEqual(610, Fibonacci(15));
+        }
+
+        public int Fibonacci(int n)
+        {
+            if (n <2) return n;
+            return (Fibonacci(n-1)+Fibonacci(n-2));
+        }
+
+    }
+}
