e36509cec507386ac5c448bf50617696d3b0467b
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-21 22:09:10 +0200

    InvertString

---
+++ InvertString/InvertString/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InvertString
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInvertString()
+        {
+            Assert.AreEqual("edcba", InvertString("abcde"));
+        }
+        string InvertString(string input)
+        {
+            if (input.Length == 1) return input;
+            return input[input.Length - 1] + InvertString(input.Substring(0, input.Length - 1));
+        }
+    }
+}
