cb9aaeaa388c8b8b48b976a433b37caf0ab54489
flaviu1 <tocaci.flaviu@yahoo.com>
2016-03-23 00:07:52 +0200

    ChangeLetterNotRecursive

---
+++ Replacement/Replacement/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Replacement
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       /* [TestMethod]
+        public void TestCangeLetterRecursiv()
+        {
+           Assert.AreEqual("Flavia", ChangeLetterRecursiv("Flaviu", 'u', "a"));
+        }
+        string ChangeLetterRecursiv(string input, char letterChange, string newLetter)
+        {
+            if (input.Length > 1)
+            {
+              
+            }
+            return "Flavia";
+        }*/
+        [TestMethod]
+        public void TestChangeLetter()
+        {
+            Assert.AreEqual("Flavia", ChangeLetterNotRecursiv("Flaviu", 'u', "a"));
+            Assert.AreEqual("FlaviaFlaviu", ChangeLetterNotRecursiv("Flaviu", 'u', "aFlaviu"));
+        }
+         
+        string ChangeLetterNotRecursiv( string input, char letterChange, string newLetter)
+        {
+            string result = string.Empty;
+            for(int i=0;i<input.Length;i++)
+            {
+                if (input[i] == letterChange)
+                {
+                    result += newLetter;
+                }
+                else
+                    result += input[i];
+            }
+            return result;
+        }
+    }
+}
