64dd20c38c3df2ac9ba6a8a5f3879808aebb0cbb
flaviu1 <tocaci.flaviu@yahoo.com>
2016-05-10 17:09:56 +0300

    NewList<T>

---
+++ List/List/IList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class List<T> : IList<T>
+    {
+        private T[] array = new T[] { };
+        private int count;
+        public int IndexOf(T item)
+        {
+            int itemIndex = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    itemIndex = i;
+                    break;
+                }
+            }
+            return itemIndex;
+        }
+
+        public void Insert(int index, T item)
+        {
+            {
+                if ((count <= array.Length) && (index < Count) && (index >= 0))
+                {
+                    count++;
+
+                    for (int i = Count - 2; i > index; i--)
+                    {
+                        array[i] = array[i - 1];
+                    }
+                    array[index] = item;
+                }
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                count--;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[count];
+            }
+            set
+            {
+                array[count] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (Count == 0)
+                Array.Resize(ref array, array.Length + 1);
+            if (array.Length <= Count && count == Count)
+                Array.Resize(ref array, array.Length * 2);
+            array[count] = item;
+            count++;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            bool inList = false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    inList = true;
+                    break;
+                }
+            }
+            return inList;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            return true;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.GetEnumerator();
+
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ List/List/UnitTest1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace List
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddListInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(3);
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void AddListString()
+        {
+            List<string> list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+            list.Add("d");
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void IndexOfInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.AreEqual(1, list.IndexOf(2));
+        }
+        [TestMethod]
+        public void IndexOfString()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            Assert.AreEqual(1, list.IndexOf("2"));
+        }
+        [TestMethod]
+        public void ClearListInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void ClearListString()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void ClearListChar()
+        {
+            List<char> list = new List<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+        [TestMethod]
+        public void RemoveAtElementFromListOfInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.RemoveAt(1);
+            Assert.AreEqual(2, list.Count);
+        }
+        [TestMethod]
+        public void RemoveAtElementFromListOfString()
+        {
+            List<string> list = new List<string>();
+            list.Add("a");
+            list.Add("a");
+            list.Add("a");
+            list.RemoveAt(1);
+            Assert.AreEqual(2, list.Count);
+        }
+        [TestMethod]
+        public void RemoveAtElementFromListOfChar()
+        {
+            List<char> list = new List<char>();
+            list.Add('a');
+            list.Add('a');
+            list.Add('a');
+            list.RemoveAt(1);
+            Assert.AreEqual(2, list.Count);
+        }
+        [TestMethod]
+        public void RemoveElementIndexNegativeFromListInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.RemoveAt(-1);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void RemoveElementIndexNegativeFromListString()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.RemoveAt(-1);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void RemoveElementIndexNegativeFromListChar()
+        {
+            List<char> list = new List<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            list.RemoveAt(-1);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void RemoveElementIndexLargerThanTheListveFromListInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.RemoveAt(4);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void RemoveElementIndexLargerThanTheListveFromListString()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.RemoveAt(4);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void RemoveElementIndexLargerThanTheListveFromListChar()
+        {
+            List<char> list = new List<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            list.RemoveAt(3);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void InsertInListInt()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Insert(2, 10);
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void InsertInListString()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Insert(2, "10");
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void InsertInListChar()
+        {
+            List<char> list = new List<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            list.Insert(2, '1');
+            Assert.AreEqual(4, list.Count);
+        }
+        [TestMethod]
+        public void InsertInListIntIndexLargerThanCountIndexLessThanZero()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Insert(-1, 10);
+            list.Insert(4, 10);
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void InsertInListStringIndexLargerThanCountIndexLessThanZero()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Insert(-1, "10");
+            list.Insert(4, "10");
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void InsertInListCharIndexLargerThanCountIndexLessThanZero()
+        {
+            List<char> list = new List<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            list.Insert(-2, '1');
+            list.Insert(4, '1');
+            Assert.AreEqual(3, list.Count);
+        }
+        [TestMethod]
+        public void ContainsListNumber()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.IsFalse(list.Contains(4));
+            Assert.IsTrue(list.Contains(2));
+        }
+        [TestMethod]
+        public void ContainsListString()
+        {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            Assert.IsFalse(list.Contains("4"));
+            Assert.IsTrue(list.Contains("2"));
+        }
+        [TestMethod]
+        public void ContainsListChar()
+        {
+            List<char> list = new List<char>();
+            list.Add('1');
+            list.Add('2');
+            list.Add('3');
+            Assert.IsFalse(list.Contains('4'));
+            Assert.IsTrue(list.Contains('2'));
+        }
+    }
+
+}
